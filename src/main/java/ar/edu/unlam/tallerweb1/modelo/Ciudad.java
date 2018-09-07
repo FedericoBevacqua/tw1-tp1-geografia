c8bf0e41ceb5dec36abc19bfa98d1f9b4a3fa774
Federico Bevacqua <fede.el.xeneize@gmail.com>
2018-09-07 19:00:59 -0300

    Agregadas a las clases @Entity

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ciudad.java
@@ -1,10 +1,12 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Ciudad {
 	
 	@Id
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -1,9 +1,11 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Ubicacion {
 	
 	@Id
